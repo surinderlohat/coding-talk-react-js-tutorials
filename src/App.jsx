b9cc499168b7dd1ef75b857820338da217188162
Surinder Singh <surinder97815@gmail.com>
2022-07-28 09:23:20 +0530

    Optimized Code for Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,105 @@
 import { useState } from "react";
 
+// input | textAra | select
+// value , onChange , options
+const InputTypes = ({ inputType = "input", ...otherProps }) => {
+  switch (inputType) {
+    case "input":
+      return (
+        <input
+          type="text"
+          value={otherProps.value}
+          onChange={(e) => otherProps.onChange(e.target.value)}
+        />
+      );
+    case "textAra":
+      return (
+        <textarea
+          type="text"
+          value={otherProps.value}
+          onChange={(e) => otherProps.onChange(e.target.value)}
+        />
+      );
+    case "select":
+      return (
+        <select
+          value={otherProps.value}
+          onChange={(e) => {
+            debugger;
+            otherProps.onChange(e.target.value);
+          }}
+        >
+          {otherProps.options?.map((x) => (
+            <option key={x} value={x}>
+              {x}
+            </option>
+          ))}
+        </select>
+      );
+    default:
+      break;
+  }
+};
+
+const InputControl = ({ label, ...otherProps }) => {
+  return (
+    <div>
+      <label>
+        {label}
+        <InputTypes {...otherProps} />
+      </label>
+    </div>
+  );
+};
+
 function App() {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [address, setAddress] = useState("");
-  const [favFruit, setFavFruit] = useState("");
+  const [user, setUser] = useState({
+    firstName: "",
+    lastName: "",
+    address: "",
+    favFruit: "",
+  });
 
   const onSubmit = (e) => {
     e.preventDefault();
-    console.log("firstName", firstName);
-    console.log("lastName", lastName);
-    console.log("address", address);
-    console.log("favFruit", favFruit);
+    console.log("user", user);
   };
+
+  const onChange = (fieldKey, value) =>
+    setUser((_user) => ({
+      ..._user,
+      [fieldKey]: value,
+    }));
 
   return (
     <div>
       <form onSubmit={onSubmit}>
-        <div>
-          <label>
-            First Name
-            <input
-              type="text"
-              value={firstName}
-              onChange={(e) => setFirstName(e.target.value)}
-            />
-          </label>
-        </div>
-        <div>
-          <label>
-            Last Name
-            <input
-              type="text"
-              value={lastName}
-              onChange={(e) => setLastName(e.target.value)}
-            />
-          </label>
-        </div>
-        <div>
-          <label>
-            Address
-            <textarea
-              type="text"
-              value={address}
-              onChange={(e) => setAddress(e.target.value)}
-            />
-          </label>
-        </div>
+        <InputControl
+          label="First Name"
+          value={user.firstName}
+          onChange={(value) => onChange("firstName", value)}
+        />
 
-        <div>
-          <label>
-            Fav Fruit
-            <select value={favFruit} onChange={(e) => setFavFruit(e.target.value)}>
-              <option value="grapefruit">Grapefruit</option>
-              <option value="lime">Lime</option>
-              <option value="coconut">Coconut</option>
-              <option value="mango">Mango</option>
-            </select>
-          </label>
-        </div>
+        <InputControl
+          label="Last Name"
+          value={user.lastName}
+          onChange={(value) => onChange("lastName", value)}
+        />
+
+        <InputControl
+          label="Address"
+          value={user.address}
+          inputType="textAra"
+          onChange={(value) => onChange("address", value)}
+        />
+
+        <InputControl
+          label="Fav Fruit"
+          value={user.favFruit}
+          inputType="select"
+          options={["A", "B", "C", "D"]}
+          onChange={(value) => onChange("favFruit", value)}
+        />
         <input type="submit" value="Submit" />
       </form>
     </div>
